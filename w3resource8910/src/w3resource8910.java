af70fab38487dab29e14fe4bc5f5552e614dacbd
patrickmylius <53932886+patrickmylius@users.noreply.github.com>
2019-09-13 06:33:26 +0200

    Add files via upload

---
+++ w3resource8910/src/w3resource8910.java
@@ -0,0 +1,16 @@
+public class w3resource8910 {
+    public static void main(String[] args) {
+
+        System.out.println("   J    a    v     v   a  ");
+        System.out.println("   J   a a    v   v   a a");
+        System.out.println("J  J  aaaaa    V V   aaaaa");
+        System.out.println(" JJ  a     a    V    a     a");
+
+        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5-4.5));
+
+        double pi = 4.0 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11));
+        System.out.println(pi);
+
+    }
+
+}
