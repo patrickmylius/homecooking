79f9269b2a3c444c3b29ddb16c57b15a2472fb08
patrickmylius <53932886+patrickmylius@users.noreply.github.com>
2019-09-13 06:32:47 +0200

    Add files via upload

---
+++ w3resource1234/src/w3resource1234.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class w3resource1234 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Hello");
+        System.out.println("Patrick Grønvold");
+
+        System.out.println(74+36);
+        System.out.println(50/3);
+
+        System.out.println(- 5 + 8 * 6);
+        System.out.println((55 + 9) % 9);
+        System.out.println(20 + - 3 * 5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
+        int w = - 5 + 8 * 6;
+        int x = (55+9) % 9;
+        int y = 20 + (- 3 * 5 / 8);
+        int z = 5 + 15 / 3 * 2 - 8 % 3;
+
+        System.out.println(w + "\n" + x + "\n" + y + "\n" +  z);
+
+        int q = 25;
+        int e = 5;
+        System.out.println(q * e);
+
+    }
+}
+
